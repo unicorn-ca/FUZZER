8fac794e9106290ff558b28d55dbc8abbc548fd7
MalayRaghuwanshi <54880001+MalayRaghuwanshi@users.noreply.github.com>
2019-10-31 04:05:23 +0530

    Add files via upload

--- basic_fuzzer.py
+++ basic_fuzzer.py
@@ -3,6 +3,16 @@ from hypothesis import given, assume, settings,HealthCheck,Verbosity, strategies
 from datetime import timedelta
 
 # add path vscode plugin
+def readp():
+    with open("payloads-sql-blind-MSSQL-INSERT.txt",'r') as infile:
+        fun = infile.readlines()
+    return fun
+
+def readp2():
+    with open("payload.txt",'r') as infile:
+        run = infile.readlines()
+    return run
+
 def read_payload():
     with open("FUZZDB_Postgres_Enumeration.txt",'r') as infile:
         blns = infile.readlines()
@@ -12,8 +22,8 @@ def handler(event, context):
     # falsification set not provided, need to produce generate for sql
     # very basic example for rest api from https://hypothesis.readthedocs.io/en/latest/examples.html
     @settings(verbosity=Verbosity.verbose,deadline=timedelta(seconds=100),max_examples=15)
-    # composite strategies, draw from total corpus of vulnerabilities
-    @given(st.sampled_from(read_payload()))
+    # composite strategies, draw from total corpus of vulnerabilities, positives
+    @given(st.sampled_from(read_payload()+readp2()+readp()))
     def fuzz(s):
         #params = {'vuln-string': vuln}
         print(type(s))
