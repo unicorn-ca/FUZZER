141bc18b9e0bd794c84fe5285594d2d49f40e36a
uint0 <czhou428420713@gmail.com>
2019-10-31 00:23:22 +1100

    Added cross account stack monitoring:

--- basic_fuzzer.py
+++ basic_fuzzer.py
@@ -16,10 +16,45 @@ def pipeline_status(job, success):
     else:
         cp_client.put_job_failure_result(jobId=job)
 
+APIENDPOINT_OUTPUT_KEY = 'ServiceEndpoint'
+SNOOPER_ROLE = 'arn:aws:iam::171350118496:role/unicorn-xaccount-cfm-stack-snooper'
+def get_account_client(client, account):
+    sts_client = boto3.client('sts')
+    creds = sts_client.assume_role(
+        RoleArn=SNOOPER_ROLE,
+        RoleSessionName='temp-chen-lambda-access',
+        DurationSeconds=900
+    )
+    
+    sess = boto3.session.Session(aws_access_key_id=creds['Credentials']['AccessKeyId'],
+                                 aws_secret_access_key=creds['Credentials']['SecretAccessKey'],
+                                 aws_session_token=creds['Credentials']['SessionToken'])
+    return sess.client(client)
+    
+
+def get_stack_api_endpoint(stack_name):
+    client = get_account_client('cloudformation', 'dev')
+    stack = client.describe_stacks(StackName=stack_name)
+
+    if len(stack['Stacks']) > 1:
+        print('Warning', 'Found more than one stack, taking [0]')
+
+    stack = stack['Stacks'][0]
+    if stack['StackStatus'] != 'CREATE_COMPLETE':
+        raise Exception('Stack is not ready')
+
+    for output in stack['Outputs']:
+        if output['OutputKey'] == APIENDPOINT_OUTPUT_KEY:
+            return output['OutputValue']
+    else:
+        raise Exception(f'Did not find "{APIENDPOINT_OUTPUT_KEY}" output')
+
 def handler(event, context):
     job_id = event['CodePipeline.job']['id']
     print(job_id)
-    print(event['actionConfiguration']['configuration']['UserParameters'])
+    stack_name = event['actionConfiguration']['configuration']['UserParameters']
+    print(stack_name)
+    api_endpoint = get_stack_api_endpoint(stack_name)
 
     # falsification set not provided, need to produce generate for sql
     # very basic example for rest api from https://hypothesis.readthedocs.io/en/latest/examples.html
